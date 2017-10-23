962ba84f6b3a2e93d058ced5a1cdb77c6fda63c8
randy-darsh <rspringer47@gmail.com>
2017-10-23 17:54:33 -0600

    update schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023193519) do
+ActiveRecord::Schema.define(version: 20171023205215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "gifs", force: :cascade do |t|
-    t.text "image_path"
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "gifs", force: :cascade do |t|
+    t.text "image_path"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_gifs_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Category.destroy_all
+Gif.destroy_all
+
+User.create(username: "user", password: "password")
+category = Category.create(name: "Randy Savage")
+category.gifs.create(image_path: "https://media.giphy.com/media/DtLEOehAWfwiY/giphy.gif")
+category.gifs.create(image_path: "https://media.giphy.com/media/lAy11oosV1cw8/giphy.gif")
+category.gifs.create(image_path: "https://media.giphy.com/media/fQ4jzmrabZw9a/giphy.gif")
+category.gifs.create(image_path: "https://media.giphy.com/media/12k3XRGaPqjdja/giphy.gif")
+category.gifs.create(image_path: "https://media.giphy.com/media/a0rBwdOIkwxEc/giphy.gif")
+category.gifs.create(image_path: "https://media.giphy.com/media/BaSHs78BU2ZYQ/giphy.gif")
+
+category_2 = Category.create(name: "Matt Foley")
+category_2.gifs.create(image_path: "https://media.giphy.com/media/i4Mgat1S2f3Co/giphy.gif")
+category_2.gifs.create(image_path: "https://media.giphy.com/media/qXKF6XHuCxFXW/giphy.gif")
+category_2.gifs.create(image_path: "https://media.giphy.com/media/EJOcCOdI69f3O/giphy.gif")
+category_2.gifs.create(image_path: "https://media.giphy.com/media/JkmxMt7lqJJa8/giphy.gif")
+category_2.gifs.create(image_path: "https://media.giphy.com/media/l378h29fpGFXGaeiY/giphy.gif")
+category_2.gifs.create(image_path: "https://media.giphy.com/media/3ov9jKkvngA56asAtG/giphy.gif")
+
+category_3 = Category.create(name: "Ric Flair")
+category_3.gifs.create(image_path: "https://media.giphy.com/media/V80llXf734WzK/giphy.gif")
+category_3.gifs.create(image_path: "https://media.giphy.com/media/125WvQGXcUfEBy/giphy.gif")
+category_3.gifs.create(image_path: "https://media.giphy.com/media/Ro6bzW9q9Bttu/giphy.gif")
+category_3.gifs.create(image_path: "https://media.giphy.com/media/yUI3a7RwLhOFy/giphy.gif")
+category_3.gifs.create(image_path: "https://media.giphy.com/media/JikFl66U6MX2U/giphy.gif")
+category_3.gifs.create(image_path: "https://media.giphy.com/media/78HKkFJxrA8jm/giphy.gif")
