7ac9fdde3f6a5888ecc61d64b2a5081269b0ed9a
randy-darsh <rspringer47@gmail.com>
2017-10-27 14:11:35 -0600

    add admin deletes category test

---
+++ spec/features/admin/admin_can_delete_a_category_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Admin visits the category index page' do
+  it 'they can delete a category' do
+    category = Category.create(name: "Thing")
+    admin = User.create(username: "admin", password: "admin", role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit categories_path
+
+    click_on "Delete"
+
+    expect(current_path).to eq(categories_path)
+    expect(page).to_not have_content("Thing")
+  end
+end
