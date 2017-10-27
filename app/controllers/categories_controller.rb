a0a1231627333aa803b669019183044e6a159cc0
randy-darsh <rspringer47@gmail.com>
2017-10-27 14:08:06 -0600

    add delete to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,11 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+    redirect_to categories_path
+  end
+
 end
