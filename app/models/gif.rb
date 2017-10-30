054301f4d5688d69dd042b0b79ca2ff77f996e87
randy-darsh <rspringer47@gmail.com>
2017-10-29 19:01:16 -0600

    update for favorites relationship

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,3 +1,5 @@
 class Gif < ApplicationRecord
   belongs_to :category
+  has_many :favorites
+  has_many :users, through: :favorites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :favorites
+  has_many :gifs, through: :favorites
 
   enum role: ["default", "admin"]
 end
