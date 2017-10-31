8d09336de42b5d6c32287ae18bde45a78db16d98
randy-darsh <rspringer47@gmail.com>
2017-10-31 10:03:37 -0600

    add methods to admin gifs controller

--- app/controllers/admin/gifs_controller.rb
+++ app/controllers/admin/gifs_controller.rb
@@ -1,2 +1,23 @@
+require 'net/http'
+require 'json'
+
 class Admin::GifsController < Admin::BaseController
+  def new
+    @gif = Gif.new
+  end
+
+  def create
+  end
+
+  def destroy
+    gif = Gif.find(params[:id])
+    gif.destroy
+    redirect_to gifs_path
+  end
+
+  private
+
+  def gif_params
+    params.require(:gif).permit(:search_term, :image_path)
+  end
 end
