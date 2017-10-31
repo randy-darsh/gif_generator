6815d62305b4580ea95a69d29229a3e74909a37c
randy-darsh <rspringer47@gmail.com>
2017-10-31 10:04:28 -0600

    update gifs controller for regular users

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,5 +1,35 @@
 class GifsController < ApplicationController
   def index
-    @categories = Category.all
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      check_user
+    else
+      @grouped_gifs = Gif.grouped_gifs.sort
+    end
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    gif = Gif.find(params["gif_id"])
+    user.favorite(gif)
+    flash[:success] = "Gif successfully favorited!"
+    redirect_to gifs_path
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    gif = Gif.find(params[:id])
+    user.unfavorite(gif)
+    redirect_to user_gifs_path(user)
+  end
+
+  private
+
+  def check_user
+    if current_user != @user
+      redirect_to root_path
+    else
+      @grouped_gifs = @user.grouped_gifs.sort
+    end
   end
 end
